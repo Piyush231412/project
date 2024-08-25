f03f5329a24764abe24cad958d5bac982f007e5e
piyush paliwal <piyushpaliwal305@gmail.com>
2024-08-26 00:08:26 +0530

    new button

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //btn
-arr=["mango","apple"];
+arr1=["mango","apple"];
 function bt()
 {
     let a=Math.floor(Math.random*2);
     console.log(arr[a]);
+}
+arr2=["potato","tomato"];
+function new_bt()
+{
+    let b=Math.floor(Math.random*2);
+    console.log(arr[b]);
 }
